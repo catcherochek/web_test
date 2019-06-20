c14096d0bdc326fe626c58a0840c7866baabd834
Klim <catcherochek@yandex.ru>
2019-06-21 00:26:04 +0300

    bat_push

--- vue/banner1/src/App.vue
+++ vue/banner1/src/App.vue
@@ -1,21 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class ="banner">
+		<div id = "iphone">
+			<img class="objimg obj1" src="./assets/img/AB2_Chuck.png"></div>
+			<p>{{msg}}</p>
+		</div>
+	</div>
   </div>
 </template>
 
@@ -24,7 +14,8 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+	startpintnum: 5,
+      msg: 'ffs'
     }
   }
 }
@@ -38,8 +29,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
-
+.objimg{
+	border: #2c3e50 1px solid;
+}
+ .obj1{
+	position: relative;
+	top:20px;
+	left:40px;
+}
 h1, h2 {
   font-weight: normal;
 }
@@ -57,4 +56,14 @@ li {
 a {
   color: #42b983;
 }
+#iphone {
+	width:750px;
+	height: 360px;
+	background-image:url('./assets/img/iphone.png');
+	background-position: center;
+	background-repeat: no-repeat;
+	display: flex;
+  	align-items: flex-start;
+}
+
 </style>
